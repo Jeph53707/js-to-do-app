87040f8c776c6bd80a2abaefcc4ccc6b54c3bf5e
Scartafist <jc53707@outlook.com>
2023-02-15 18:07:31 -0500

    added JS

--- script.js
+++ script.js
@@ -3,24 +3,63 @@
 // 1) Save the values / DOM elements we will need to interact with
     
     // let's query the DOM for the form element and save it within a variable
+    const formElement = document.querySelector('form');
 
     // let's query the DOM for the input element and save it within a variable
+    const inputElement = document.querySelector('#toDoItem');
+
+    // let's query the DOM for the ul element and save it within a variable
+    const ulElement = document.querySelector('ul');
 
 
 // 2) Attach a submit event listener to the form element
     // pass in 2 arguments to the event listener method:
-        // a) the event you are listening for
+        // a) the event you are listening for (in 'string' format)
         // b) the callback function which will hold the logic we wish run ONCE the event "is heard" (AKA occurs on the page)
+            // every time an event occurs, an event object is generated (we're going pass that obj into the scope of the callback func)
+    formElement.addEventListener('submit', function(event) {
 
-    // save the text value from the input within a variable
+        // the default behaviour of a form is to refresh the page SO we need to tell it NOT to carry out its default behaviour
+        event.preventDefault();
+
+        // log out the unique event object that is generated when the submit event is occurrred
+        console.log(event);
+
+        // save the entered text value from the input within a variable
+            // NOTE: any info entered into an input element will always be available at the value property
+        const userToDo = inputElement.value;
+
+        // NOTE: form inputs ALWAYS return values as strings
+            // if the user submits the form without entering anything, that returns an empty string
+        
+        // IF the user submits a todo (check whether the value of the input is NOT empty) then:
+            // STRETCH ERROR-HANDLING GOAL (courtesy of Roberto:) how do we avoid adding empty strings with multiple space characters (probably RegEx)
+        if (userToDo !== "") {
+
+            console.log(userToDo);
+
+            // create a li element
+            const liElement = document.createElement('li');
+          
+                // add a FA icon to the li by reassigning the value of that property to be the icon HTML
+                liElement.innerHTML = '<i class="fa-regular fa-square"></i>';
+               
+                // add the text from the todo that the user entered to the li
+                const toDoText = document.createTextNode (userToDo);
+                console.log (toDoText);
+                liElement.appendChild(toDoText);
+                ulElement.appendChild(liElement);
+            
+              
     
-    // IF the user submits a todo (check whether the value of the input is NOT empty) then:
-        // create a li element
-            // add a FA icon to the li
-            // add the text from the todo that the user entered to the li
+            // append the li element to the ul element
+        } else {
+            // ELSE alert the user to please submit a valid to do
+            alert('Please enter a valid task! Do not leave the input empty.')
+        }
 
-        // append the li element to the ul element
-    // ELSE alert the user to please submit a valid to do
+    })
+
 
 
 // 3) KENZIE'S STEP: how do we track when the task is done?
